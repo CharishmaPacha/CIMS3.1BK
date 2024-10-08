597ffa2cbeadf68d8f0b64881554593ed6b65044
VijayMadhyanapu <vijay.madhyanapu@cloudimsystems.com>
2024-10-08 15:47:42 +0530

    BK-1113 Config changes to use CIMS SQL from Git repo

--- CIMSDE/SQLInit/_init_UpgradeAll_V3DE.sql
+++ CIMSDE/SQLInit/_init_UpgradeAll_V3DE.sql
@@ -5,14 +5,15 @@
 
   Date        Person  Comments
 
+  2024/10/08  VM      Config changes as CIMS SQL is on Git (BK-1113)
   2024/02/28  VM      Moved to run Inits from CIMSDECreate.sql to _init_UpgradeAll_V3DE.sql (CIMSV3-3430)
   2024/01/30  VM      Initial Revision (HA-3952)
 ------------------------------------------------------------------------------*/
 /* Apply DE related init changes to a CIMS DE Database */
 
 /* Inits */
-Input ..\SQLInit_V3\Main\init_BusinessUnits.sql;
-Input ..\SQLInit_DE\Main\init_DE_Sequences.sql;
-Input ..\SQLInit_DE\Main\init_DE_Control.sql;
+Input ..\..\SQLInit\Main\init_BusinessUnits.sql;
+Input .\Main\init_DE_Sequences.sql;
+Input .\Main\init_DE_Control.sql;
 
 Go
