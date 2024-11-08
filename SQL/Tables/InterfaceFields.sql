f04371f916346432e1dbc68ccb6be6f52c28d2ab
magbulshaik-cims <magbul.shaik@foxfireindia.com>
2024-11-08 15:06:17 +0530

    CIMSV3-2130 Import File: Import SKU Info from file

--- SQL/Tables/InterfaceFields.sql
+++ SQL/Tables/InterfaceFields.sql
@@ -5,8 +5,9 @@
 
   Date        Person  Comments
 
+  2023/07/26  VS      InterfaceFields: Added Operation (JLFL-394)
   2020/11/14  AY      InterfaceFields: Changed datatype of FieldType as we could use domain name (CIMSV3-1120)
-  2020/11/12  TK      InterfaceFields: Added FielddefaultValue (CID-1498)
+  2020/11/12  TK      InterfaceFields: Added FieldDefaultValue (CID-1498)
   2015/11/04  KN      Added InterfaceFields table definition (GNCLMS-4).
 ------------------------------------------------------------------------------*/
 
@@ -14,18 +15,31 @@ Go
 
 /*------------------------------------------------------------------------------
  Table: InterfaceFields
+
+  Operations: CSV of these values: Upload,Import,Augment,Insert,Update
+              Upload: Fields that are expected to be uploaded from the file
+              Import: Fields that are to be imported into the actual tables
+              Augment: Fields which are not uploaded from the file but added to the
+                       hash table during FileImport either for processing, or validation
+                       or for applying a default value.
+              Insert: Fields that are to be inserted.
+              Update: Fields that are to be updated.
+              All applicable options would be included in the Operation
+
+  At present Insert/Update are not used for FileImport
 ------------------------------------------------------------------------------*/
 Create Table InterfaceFields (
     RecordId                 TRecordId      identity (1,1) not null,
 
     ProcessName              TName,         /* Import, Export or specific Import/Export process */
     DataSetName              TName,         /* Table name/Record type to group fields */
+    Operations               TString,       /* FileImport, Upload, Insert, Update */
 
     FieldName                TName,
     ExternalFieldName        TName,
     FieldType                TName,         /* string, integer, datetime, date */
     FieldWidth               TInteger,
-    FielddefaultValue        TDescription,
+    FieldDefaultValue        TDescription,
     Justification            TName,         /* left, right */
     PadChar                  Char,          /* char to use used for padding */
 
